27bc7413f0dce38bd2eddfc65e3b0d4c59a1abb6
saijami <saijami38@gmail.com>
2025-04-29 09:33:07 +0530

    code removed

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,57 +36,6 @@ let userSecrets = {};
 // Routes
 app.use("/api/auth", authRoutes);
 
-app.post("/generate-2fa", (req, res) => {
-  const { username } = req.body;
-
-  const key = Totp.generateKey({ issuer: "sai", user: username });
-
-  userSecrets[username] = key;
-
-  res.json({
-    secret: key.secret,
-    url: key.url,
-  });
-});
-
-app.post("/verify-2fa", (req, res) => {
-  try {
-    const { username, token } = req.body;
-
-    if (!username || !token) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Username and token are required." });
-    }
-
-    const userSecretData = userSecrets[username];
-
-    if (!userSecretData || !userSecretData.secret) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found or secret missing." });
-    }
-
-    const isTokenValid = Totp.validate({
-      passcode: token,
-      secret: userSecretData.secret,
-    });
-
-    if (!isTokenValid) {
-      return res.status(400).json({ success: false, message: "Invalid OTP." });
-    }
-
-    return res
-      .status(200)
-      .json({ success: true, message: "OTP verified successfully!" });
-  } catch (error) {
-    console.error("Error verifying 2FA token:", error);
-    return res
-      .status(500)
-      .json({ success: false, message: "Internal server error." });
-  }
-});
-
 // Listen app
 const port = process.env.PORT || 7001;
 app.listen(port, () => {
