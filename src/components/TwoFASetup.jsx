06587335e57695b4f05c1ab3c2732f884bdea605
saijami <saijami38@gmail.com>
2025-04-30 09:18:45 +0530

    TwoFASetup ui

---
+++ src/components/TwoFASetup.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export default function TwoFASetup({ onSetupComplete }) {
+  const [message, setMessage] = useState("");
+  const copyClipBoard = async () => {
+    await navigator.clipboard.writeText();
+    setMessage("Secret copied to clipboard!");
+  };
+  return (
+    <div className="bg-white rounded-md p-4">
+      <h2 className="text-xl text-center font-medium mb-4">Enable 2FA</h2>
+      <p className="text-center text-gray-600 text-sm mb-4 pl-6 pr-6">
+        Scan the QR code using your authenticator app
+      </p>
+      <div className="flex justify-center mb-4">
+        <img alt="2FA QR code" className="w-40 h-40 border rounded-sm" />
+      </div>
+      <div className="flex items-center text-xs text-gray-500">
+        <div className="flex-grow border-t"></div>
+        <span className="px-2">Enter code manually</span>
+        <div className="flex-grow border-t"></div>
+      </div>
+      <div className="mb-6">
+        {message && <p className="text-green-600 text-sm mb-3">{message}</p>}
+        <input
+          readOnly
+          defaultValue=""
+          value=""
+          className="w-full rounded mt-2 text-xs text-gray-600 p-4 "
+          onClick={copyClipBoard}
+        />
+      </div>
+      <button
+        onClick={onSetupComplete}
+        className="w-full bg-blue-500 text-white py-2 rounded-md"
+      >
+        Continue to Verification
+      </button>
+    </div>
+  );
+}
--- src/pages/Setup2FA.jsx
+++ src/pages/Setup2FA.jsx
@@ -1,6 +1,11 @@
+import TwoFASetup from "@/components/TwoFASetup";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Setup2FA() {
-  console.log("Setup2FA");
-  return <div>Setup2FA</div>;
+  const navigate = useNavigate();
+  const handleSetupComplete = () => {
+    navigate("/verify-2fa");
+  };
+  return <TwoFASetup onSetupComplete={handleSetupComplete} />;
 }
