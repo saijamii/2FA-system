5e94d22897491ea1fe4cd6ed6e6bdff43eb92bd9
saijami <saijami38@gmail.com>
2025-05-03 09:44:34 +0530

    ref and keys

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,31 +1,57 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function OtpInput() {
   const OTP_DIGIT_COUNT = 6;
   const [inputArr, setInputArr] = useState(new Array(OTP_DIGIT_COUNT).fill(""));
   console.log(inputArr);
   const arrRef = useRef([]);
-  console.log(arrRef?.current);
+
+  useEffect(() => {
+    arrRef?.current[0]?.focus();
+  }, []);
+
   const handleOnchange = (value, index) => {
     if (isNaN(value)) return;
+    const newValue = value.trim();
     const newArr = [...inputArr];
-    newArr[index] = value.slice(-1);
+    newArr[index] = newValue.slice(-1);
     setInputArr(newArr);
+
+    if (index < OTP_DIGIT_COUNT - 1) {
+      newValue && arrRef?.current[index + 1]?.focus();
+    }
+  };
+
+  const handleOnKeyDown = (e, index) => {
+    if (e.key === "ArrowRight") {
+      e.preventDefault();
+      arrRef?.current[index + 1]?.focus();
+    } else if (e.key === "ArrowLeft") {
+      e.preventDefault();
+      if (index > 0) {
+        arrRef?.current[index - 1]?.focus();
+      }
+    } else if (e.key === "Backspace") {
+      !e.target.value && arrRef?.current[index - 1]?.focus();
+    }
   };
 
   return (
-    <div className="flex flex-row w-md">
-      {inputArr.map((e, index) => (
-        <input
-          key={index}
-          ref={(input) => {
-            arrRef.current[index] = input;
-          }}
-          className="h-12 w-12 text-3xl text-center m-0.2 border"
-          value={inputArr[index]}
-          onChange={(e) => handleOnchange(e.target.value, index)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-row w-md">
+        {inputArr.map((e, index) => (
+          <input
+            key={index}
+            ref={(input) => {
+              arrRef.current[index] = input;
+            }}
+            className="h-12 w-12 text-3xl text-center m-0.2 border"
+            value={inputArr[index]}
+            onChange={(e) => handleOnchange(e.target.value, index)}
+            onKeyDown={(e) => handleOnKeyDown(e, index)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
